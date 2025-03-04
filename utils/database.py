cda13168ab2368a37507a61c4b803e9232a70ff9
Artyom <net775@yandex.ru>
2025-03-04 15:31:32 +0300

    Добавлен контекстный менеджер для работы с БД

--- utils/database.py
+++ utils/database.py
@@ -1,5 +1,6 @@
 import asyncpg
 from date.config import POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB
+from contextlib import asynccontextmanager
 
 #Создает подключение к базе данных
 async def create_connection():
@@ -11,33 +12,43 @@ async def create_connection():
         database=POSTGRES_DB,
     )
 
+# Добавляем контекстный менеджер для соединения с БД
+@asynccontextmanager
+async def get_connection():
+    """Контекстный менеджер для работы с БД."""
+    conn = await create_connection()
+    try:
+        yield conn
+    finally:
+        await conn.close()
+
 #Создает таблицы в базе данных, если они не существуют
 async def create_tables():
-    conn = await create_connection()
-    await conn.execute("""
-        CREATE TABLE IF NOT EXISTS support_requests (
-            id SERIAL PRIMARY KEY,
-            user_id BIGINT NULL,
-            user_username TEXT NULL,
-            name TEXT NULL,
-            email TEXT NULL,
-            message TEXT NULL,
-            admin_id BIGINT NULL,
-            admin_name TEXT NULL,
-            document_id TEXT,
-            photo_id TEXT,
-            document_path TEXT,
-            photo_path TEXT,
-            created_at TIMESTAMP DEFAULT NOW()
-        );
-    """)
-    await conn.execute("""
-        CREATE TABLE IF NOT EXISTS support_responses (
-            id SERIAL PRIMARY KEY,
-            request_id INT REFERENCES support_requests(id),
-            admin_id BIGINT NOT NULL,
-            message TEXT NOT NULL,
-            created_at TIMESTAMP DEFAULT NOW()
-        );
-    """)
-    await conn.close()
+    """Создает необходимые таблицы в БД."""
+    async with get_connection() as conn:
+        await conn.execute("""
+            CREATE TABLE IF NOT EXISTS support_requests (
+                id SERIAL PRIMARY KEY,
+                user_id BIGINT NULL,
+                user_username TEXT NULL,
+                name TEXT NULL,
+                email TEXT NULL,
+                message TEXT NULL,
+                admin_id BIGINT NULL,
+                admin_name TEXT NULL,
+                document_id TEXT,
+                photo_id TEXT,
+                document_path TEXT,
+                photo_path TEXT,
+                created_at TIMESTAMP DEFAULT NOW()
+            );
+        """)
+        await conn.execute("""
+            CREATE TABLE IF NOT EXISTS support_responses (
+                id SERIAL PRIMARY KEY,
+                request_id INT REFERENCES support_requests(id),
+                admin_id BIGINT NOT NULL,
+                message TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT NOW()
+            );
+        """)
